cbcb386de0e50baaf25605e97aa35871d099e274
DronanPC <DronanPC@DESKTOP-CU4JR9B>
2018-05-11 12:45:07 -0700

    KNN

--- Regression/Rregression.py
+++ Regression/Rregression.py
@@ -27,6 +27,7 @@ df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
 df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close'])/ df['Adj. Close']*100
 df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open'])/ df['Adj. Open']*100
 
+#          price          X           X              X
 df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
 
 forecast_col = 'Adj. Close'
@@ -35,15 +36,15 @@ df.fillna(-99999, inplace = True) #replace non
 
 #math.ceil - Return the ceiling of x as a float, 
 #the smallest integer value greater than or equal to x.
-forecast_out = int(math.ceil(0.01*len(df))) 
+forecast_out = int(math.ceil(0.1*len(df))) 
 print('Prediction for {} days'.format(forecast_out)) 
 
 df['label'] = df[forecast_col].shift(-forecast_out)
 
 x = np.array(df.drop(['label'],1))
 x = preprocessing.scale(x)
-x = x[:-forecast_out]
 x_lately = x[-forecast_out:]
+x = x[:-forecast_out]
 
 df.dropna(inplace = True)
 y = np.array(df['label'])
