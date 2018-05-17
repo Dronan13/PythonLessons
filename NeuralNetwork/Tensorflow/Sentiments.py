d9f0f59d3cae0d9ad38aec38f8428c655345244c
Dronan13 <dronan13@gmail.com>
2018-05-17 12:42:04 -0700

    sent

--- NeuralNetwork/Tensorflow/Sentiments.py
+++ NeuralNetwork/Tensorflow/Sentiments.py
@@ -15,7 +15,44 @@ def create_lexicon(pos,neg):
         with open(fi, 'r') as f:
             contents = f.readlines()
             for l in contents[:hm_lines]:
-                all_words = word_tokenize(l)
+                all_words = word_tokenize(l.lower())
                 lexicon += list(all_words)
-                
+    
+    lexicon = [lemmatizer.lemmatize(i) for i in lexicon]
+    w_counts = Counter(lexicon)
+    
+    l2 = []
+    for w in w_counts:
+        if 1000> w_counts[w] > 50:
+            l2.append(w) 
+            
+    return l2
+    
+def sample_handler(sample, lexicon, classification):
+    featureset = []
+    
+    with open(sample, 'r') as f:
+        contents = f.readlines()
+        for l in contents[:hm_lines]:
+            current_words = word_tokenize(l.lower())
+            current_words = [lemmatizer.lemmatize(i) for i in current_words]
+            features = np.zeros(len(lexicon))
+            for word in current_words:
+                if word.lower() in lexicon:
+                    index_value = lexicon.index(word.lower())
+                    features[index_value] += 1
+                    
+            features = list(features)
+            featureset.append([features, classification])
+            
+    return featureset
 
+def create_features_sets_and_labels(pos,neg,test_size =0.1):
+    lexicon = create_lexicon(pos, neg)
+    features = []
+    features += sample_handler('pos.txt', lexicon, [1, 0])
+    features += sample_handler('neg.txt', lexicon, [0, 1])
+    random.shuffle(features)
+    
+                
+    
